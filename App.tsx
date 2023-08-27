9e2f02f3292354dfab3ab760ad51276bee35fa6f
castromaciel <castromaciel00@gmail.com>
2023-08-27 20:02:59 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+
+interface navigationAppProps {}
+
+const App: FC<navigationAppProps> = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
