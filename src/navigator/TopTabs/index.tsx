6cafad0916884ab20e71b586dcce523699e9592d
castromaciel <castromaciel00@gmail.com>
2023-11-30 00:28:57 -0300

    customize top tab options

--- src/navigator/TopTabs/index.tsx
+++ src/navigator/TopTabs/index.tsx
@@ -1,7 +1,9 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { FC } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { AlbumsScreen, ChatScreen, ContactsScreen } from '../../screens';
+import { colors } from '../../theme/appTheme';
 
 type Screens = {
   'Albums': undefined
@@ -13,8 +15,46 @@ const Tab = createMaterialTopTabNavigator<Screens>();
 interface TopTabsProps {}
 
 const TopTabs: FC<TopTabsProps> = () => {
+  const { top: paddingTop } = useSafeAreaInsets()
+
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: colors.white,
+      }}
+      screenOptions={({ route }) => ({
+        tabBarPressColor: colors.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: colors.primary,
+        },
+        tabBarStyle: {
+          shadowColor: 'transparent',
+        },
+        tabBarIcon: ({ color }) => {
+          
+          let iconName = '';
+
+          switch(route.name) {
+            case 'Albums':
+              iconName = 'Al';
+              break;
+            case 'Chat':
+              iconName = 'Ch';
+              break;
+            case 'Contacts':
+              iconName = 'Ca';
+              break;
+          }
+
+          return <Text style={{ color: colors.primary }}>{iconName}</Text>
+        }
+      })}
+      style={{
+        paddingTop
+      }}
+    >
       <Tab.Screen name="Albums" component={AlbumsScreen} />
       <Tab.Screen name="Chat" component={ChatScreen} />
       <Tab.Screen name="Contacts" component={ContactsScreen} />
