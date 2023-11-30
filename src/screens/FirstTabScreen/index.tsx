b10a1f6344a2d239dc6a440b1c1baa97ac4922a7
castromaciel <castromaciel00@gmail.com>
2023-11-30 01:19:59 -0300

    install ion icons

--- src/screens/FirstTabScreen/index.tsx
+++ src/screens/FirstTabScreen/index.tsx
@@ -1,18 +1,35 @@
 import { FC, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { globalTheme } from '../../theme/appTheme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { colors, globalTheme } from '../../theme/appTheme'
 
 interface FirstTabScreenProps {}
 
 const FirstTabScreen: FC<FirstTabScreenProps> = () => {
+
+  const { top: paddingTop } = useSafeAreaInsets()
 
   useEffect(() => {
     console.log('First tab screen')
   }, [])
 
   return (
-    <View style={globalTheme.appMargin}>
-      <Text>FirstTabScreen</Text>
+    <View style={{
+      ...globalTheme.appMargin,
+      paddingTop
+    }}>
+      <Text>Icons</Text>
+
+      <Text>
+        <Icon name="airplane-outline" size={80} color={colors.primary} />
+        <Icon name="logo-linkedin" size={80} color={colors.primary} />
+        <Icon name="logo-tux" size={80} color={colors.primary} />
+        <Icon name="logo-amazon" size={80} color={colors.primary} />
+        <Icon name="logo-react" size={80} color={colors.primary} />
+        <Icon name="shirt-outline" size={80} color={colors.primary} />
+      </Text>
+      
     </View>
   )
 }
